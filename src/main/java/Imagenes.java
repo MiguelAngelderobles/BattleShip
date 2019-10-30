a81ece01346513f293547d7e6c890f7fe93830e1
MiguelAngelderobles <miguel.angelderobles@hotmail.com>
2019-10-30 15:05:57 -0300

    Sin incluir audio problemas de enrutamiento

--- src/main/java/Imagenes.java
+++ src/main/java/Imagenes.java
@@ -4,8 +4,15 @@ public class Imagenes {
 
     public Image cargarTablero(){
         return Toolkit.getDefaultToolkit().createImage("/home/miguel/asd/src/main/java/Tablero.jpg");
+          //resource s de ahi ver mañana
     }
     public Image cargarPortada(){
         return Toolkit.getDefaultToolkit().createImage("/home/miguel/asd/src/main/java/Portada.jpg");
+    }
+    public Image cargarTableroDuo(){
+        return Toolkit.getDefaultToolkit().createImage("/home/miguel/asd/src/main/java/TableroDual.jpg");
+    }
+    public Image cargarPortadaDuo(){
+        return Toolkit.getDefaultToolkit().createImage("/home/miguel/asd/src/main/java/PortadaDual.jpg");
     }
 }
